e91397c39f5601c587e4a79ccf009d2124a4074c
Anhad Gill <gillanhad23@gmail.com>
2018-02-02 02:45:31 +0000

    Adds looping exercises

---
+++ agecalculator.js
@@ -0,0 +1,4 @@
+function ageCalculator (name; yearOfBirth; currentYear) {
+  console.log($name + " is " + ($currentYear-$yearOfBirth) + " years old." );
+}
+console.log(ageCalculator("Miranda", 1983, 2015));
